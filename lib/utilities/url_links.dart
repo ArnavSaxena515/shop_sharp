c4290124681b77fdfd2c026d86ccd1e845e12984
Arnav Saxena <68912628+ArnavSaxena515@users.noreply.github.com>
2022-04-30 23:56:09 +0530

    Update url_links.dart

--- lib/utilities/url_links.dart
+++ lib/utilities/url_links.dart
@@ -1,10 +1,8 @@
-const databaseURL = "https://shop-sharp-default-rtdb.asia-southeast1.firebasedatabase.app/";
-//const signUpURL = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
-//const signInURL = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
-// ignore: constant_identifier_names
-const API_KEY = "AIzaSyAGzTP720XZI0v0-74CGLxsTS9V0mLTvm0";
+const databaseURL = "YOUR_FIREBASE_REALTIME_DATASTORE_URL_HERE";
+const API_KEY = "YOUR_API_KEY";
 enum authAction { signUp, login }
 
 String generateURL(authAction action) {
+  // URL FROM OFFICIAL FIREBASE API DOCS
   return "https://identitytoolkit.googleapis.com/v1/accounts:${action == authAction.signUp ? "signUp" : "signInWithPassword"}?key=$API_KEY";
 }
